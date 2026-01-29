993373107cb3b284a2aa1364082e405b4f3216b4
ZBencevic <zvonimir.bencevic@gmail.com>
2026-01-29 22:03:09 +0100

    Renaming file and adding new project

---
+++ PROJECT 2 - Hello You!/hello_you.py
@@ -0,0 +1,12 @@
+# Ask user for name
+name = input("What is your name? ")
+# Ask user for age
+age = input("How old are you? ")
+# Ask user for city
+city = input("Where are you from? ")
+# Ask user what they enjoy
+hobby = input("What do you enjoy doing? ")
+# Create output text
+output = f"You are {name} and you are {age} years old. You live in {city} and you enjoy doing {hobby}."
+# Print output to screen
+print(output)
