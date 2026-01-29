e700859dc728a99cd9f58d60765635f2bc08d17a
ZBencevic <zvonimir.bencevic@gmail.com>
2026-01-29 21:39:37 +0100

    Adding difficulty mechanism to a health potion

--- PROJECT 1 - Crafting a Health Potion/main.py
+++ PROJECT 1 - Crafting a Health Potion/main.py
@@ -1,7 +1,14 @@
 import random
 
 health = 50
-health_potion = random.randint(25, 50)
+
+easy = 1
+medium = 2
+hard = 3
+
+difficulty = hard
+
+health_potion = int(random.randint(25, 50) / difficulty)
 
 health += health_potion
 
